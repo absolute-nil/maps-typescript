94fa28c8de04bbe896b5091da800b1cd86e320c0
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-05 19:13:33 +0530

    add mappable interface and add maker function

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,12 +1,30 @@
+// instructions to every other class on how to be an argument to add marker
+interface Mappable {
+  location: {
+    lat: number,
+    lng: number
+  }
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
-  constructor(divId:string) {
+  constructor(divId: string) {
     this.googleMap = new google.maps.Map(document.getElementById(divId), {
       zoom: 1,
       center: {
         lat: 0,
         lng: 0
+      }
+    })
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
       }
     })
   }
